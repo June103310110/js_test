5f6107fd6cd27123720886d759a66652fa9d583f
root <root@june-minecraft.asia-east1-b.c.hopeful-vim-312006.internal>
2021-06-06 05:24:08 +0000

    update bash

--- bash.sh
+++ bash.sh
@@ -5,3 +5,6 @@ mkdir minecraft
 cd minecraft
 wget https://launcher.mojang.com/v1/objects/35139deedbd5182953cf1caa23835da59ca3d7cd/server.jar
 java -Xmx8192M -Xms1024M -jar server.jar nogui
+sed -i "s/eula=false/eula=true/g" eula.txt
+sed -i "s/online-mode=true/online-mode=false/g" server.properties
+
