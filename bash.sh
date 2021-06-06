1e0556c03eebafc953420a485e34a64aabac291e
June103310110 <32012425+June103310110@users.noreply.github.com>
2021-06-06 13:05:22 +0800

    Delete bash.sh

--- bash.sh
+++
@@ -1,9 +0,0 @@
-sudo su
-yes | apt-get update
-yes | apt-get install default-jre
-screen
-cd /home
-mkdir minecraft
-cd minecraft
-wget https://launcher.mojang.com/v1/objects/35139deedbd5182953cf1caa23835da59ca3d7cd/server.jar
-java -Xmx8192M -Xms1024M -jar server.jar nogui
